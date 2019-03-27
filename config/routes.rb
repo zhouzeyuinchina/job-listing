f7088bef178f60ed3dced459fad03aba259d8ccf
周泽宇 <zhouzeyuinchina@gmail.com>
2019-03-27 16:00:31 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :jobs
-  root 'jobs#index'
-
-
-    namespace :admin do
-      resources :jobs do
-        member do
-          post :publish
-          post :hide
-        end
+  namespace :admin do
+    resources :jobs do
+      member do
+        post :publish
+        post :hide
       end
+
+      resources :resumes
     end
+  end
+
+  resources :jobs do
+    resources :resumes
+  end
+
+  root 'jobs#index'
 end
